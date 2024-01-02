88288c87bffb7d67ebd884249c01599969bcbe88
Krishna zade <114551368+Krishcode264@users.noreply.github.com>
2024-01-02 11:59:36 +0530

    Update meet.tsx

--- web/app/components/webRTC/meet.tsx
+++ web/app/components/webRTC/meet.tsx
@@ -132,7 +132,7 @@ if(peerConnection){
   const handleSocketConnection = (newUser: User): Promise<void> => {
     return new Promise((resolve, rejecet) => {
       try {
-        const newsocket = io("http://16.171.0.192/", {
+        const newsocket = io("http://localhost:8080", {
           path: "/socket",
           transports: ["websocket"],
         });
